1833ec77e756ad57febb3ecc628d0c8c06b0d223
aoi-lucario <petersbw123@gmail.com>
2017-06-03 12:27:52 +0900

    add func.scaledown in init.py

--- init.py
+++ init.py
@@ -98,7 +98,7 @@ def sq_distance(x, y):
     return ((x-y)**2).mean()
 
 
-def k_means(data, k, distance=sim_pearson, bias=1):
+def k_means(data, k, distance=sim_pearson, bias=-1):
     user_len = data.shape[0]
     item_len = data.shape[1]
 
@@ -121,7 +121,7 @@ def k_means(data, k, distance=sim_pearson, bias=1):
             min_distance = 9999
             min_point = k+1
             for j in range(k):
-                d = abs(distance(data[i], points[j])-bias)
+                d = abs(distance(data[i], points[j])+bias)
                 if d < min_distance:
                     min_distance = d
                     min_point = j
@@ -171,14 +171,77 @@ def k_means(data, k, distance=sim_pearson, bias=1):
 
     # logging.debug("k points: %s" % str(points))
     return points
-    pass
 
+
+# create a 2-dim graph of users
+def scaledown(data, distance=sim_pearson, dis_bias=-1, move_rate=0.01, max_loop=1000):
+    logging.info("function scaledown is runing...")
+
+    user_len = data.shape[0]
+    item_len = data.shape[1]
+    """
+    # real distance = pearson distance, shape = (user_len, user_len)
+    real_dis = np.zeros((user_len, user_len), dtype=np.float32)
+    for i in range(user_len):
+        for j in range(i, user_len):
+            real_dis[i][j] = abs(distance(data[i], data[j]) + dis_bias)
+        print("init real distance: %d" % i)
+
+    np.save("real_dis.npy", real_dis)
+    """
+    real_dis = np.load("real_dis.npy")
+
+    # 2-dim location of users, shape = (user_len, 1)
+    local = [(np.random.random(), np.random.random()) for i in range(user_len)]
+    # range: (0, 1) -> (0, 2)
+    local = np.array(local, dtype=np.float32)*2
+
+    # euclid distance
+    euclid_dis = [[0 for j in range(user_len)] for i in range(user_len)]
+    euclid_dis = np.array(euclid_dis, dtype=np.float32)
+
+    logging.debug("scaledown: init complete...")
+    for m in range(max_loop):
+        # euclid distance
+        for i in range(user_len):
+            for j in range(i, user_len):
+                euclid_dis[i][j] = np.sqrt(sum((local[i]-local[j])**2))
+
+        move = np.zeros((user_len, 2), dtype=np.float32)
+        error_sum = 0
+        last_error = 0
+
+        # calculate move distance
+        for i in range(user_len):
+            for j in range(i, user_len):
+                if i == j: continue
+                error = (euclid_dis[i][j] - real_dis[i][j])/(real_dis[i][j]+0.1)
+
+                move[i][0] += ((local[i][0] - local[j][0]) / (euclid_dis[i][j]+0.1)) * error
+                move[i][0] += ((local[i][1] - local[j][1]) / (euclid_dis[i][j]+0.1)) * error
+
+                error_sum += error
+
+        logging.debug("loop: %d, sum of error: %f" % (m, error_sum))
+        if last_error and last_error < error_sum:
+            logging.warning("loop: %d, last_error < error_sum !! loop will break!" % m)
+            break
+        else:
+            last_error = error_sum
+
+        # move
+        for i in range(user_len):
+            local[i] -= move_rate*move[i]
+
+    np.save("graph.npy", local)
+    return local
 
 ###########################
 # run
 
 if __name__ == "__main__":
     # data2npy()
+    data_set = load_data()
     """
     print(sim_pearson([], []))
     print(sim_pearson([1, 2, 3, 4, 5], [2, 3, 4, 5, 6]))
@@ -187,11 +250,14 @@ if __name__ == "__main__":
     print(sim_pearson([1, 2, 5, 4, 3], [6, 1, 5, 7, 9]))
     print(sim_pearson([1, 1, 2, 1, 1], [1, 1, 2, 1, 1]))
     print(sim_pearson([1, 2, 1, 0, 0], [1, 2, 0, 0, 0]))
-    """
 
-    data_set = load_data()
+
+
     # ps = k_means(data_set, 5, distance=sq_distance, bias=0)
     ps = k_means(data_set, 5)
     print("k-means end!")
     for core in ps:
         print(str(core[:100]))
+    """
+    d = scaledown(data_set)
+    print(d)
